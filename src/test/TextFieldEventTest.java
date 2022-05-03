9421b2292c5a5fd2d48ce0e1773b6a009a767c6f
Chango961204 <metallicaluis73@gmail.com>
2022-05-03 08:53:08 -0500

    Pruebas Unitarias TextFIeldTest

---
+++ src/test/TextFieldEventTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;
+
+public class TextFieldEventTest {
+    @Test
+ public void it_returns_correct_format(){
+        assertTrue(true);
+
+    }
+
+    
+    @Test
+ public void it_returns_correct_format_email(){
+        assertTrue(true);
+
+    }  
+
+    
+}
