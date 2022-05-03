1e4b4e2b25a229bc9e02d6cc822d7a9f1631985a
Chango961204 <metallicaluis73@gmail.com>
2022-05-03 08:43:59 -0500

    Prueba Unitaria RenderCHeckBoxTest

---
+++ src/test/Render_CheckBoxTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class Render_CheckBoxTest {
+    @Test
+    public void it_returns_a_component(){
+        assertTrue(true);
+        
+    }
+    
+}
