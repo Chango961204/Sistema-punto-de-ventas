d9b3dc42caec688bc3a132cd6f4b6b46909175d1
Chango961204 <metallicaluis73@gmail.com>
2022-05-03 01:00:35 -0500

    Prueba unitaria cargar imagen

---
+++ src/test/CargarImagenesTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;
+
+public class CargarImagenesTest {
+    @Test
+    public void it_returns_bytes_in_array() {
+        assertTrue(true);
+    }
+  
+    
+}
