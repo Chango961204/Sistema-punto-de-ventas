f23b61e55a4b86b8d6b6972b2ac5f909fd05c228
Chango961204 <metallicaluis73@gmail.com>
2022-05-03 07:29:52 -0500

    EncriptarTest

---
+++ src/test/EncriptarTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class EncriptarTest {
+    @Test
+    public void it_returns_a_String() {
+        assertTrue(true);
+    }
+    @Test
+    public void it_returns_a_description(){
+        assertTrue(true);
+    }
+}
